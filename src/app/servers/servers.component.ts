a4461977636589c6bb0344c0fdcdf0d1be3a3eac
Anshul Jaiswal <anshul.a.jaiswal@gmail.com>
2023-06-07 10:44:26 +0530

    add if condition in app

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,24 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent implements OnInit{
+export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No Server was created';
   serverName: string = '';
-  constructor(){
+  serverCreated: boolean = false;
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    },5000)
+    }, 5000);
   }
-ngOnInit(): void {
-}
-onCreateServer(){
-  this.serverCreationStatus = 'Sever was created!';
-}
-onUpdateServer(event: Event){
-  this.serverName = (<HTMLInputElement>event.target).value
-
-}
+  ngOnInit(): void {}
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Sever was created! and name is ' + this.serverName;
+  }
+  onUpdateServer(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
